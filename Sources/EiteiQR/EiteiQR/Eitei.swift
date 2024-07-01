ab0c40f21632c0ef8bfd97f45e0f2013ba2488b5
damao <dadada.maomaomao@gmail.com>
2024-07-01 12:58:55 +0900

    SPM image loader

--- Sources/EiteiQR/EiteiQR/Eitei.swift
+++ Sources/EiteiQR/EiteiQR/Eitei.swift
@@ -20,28 +20,21 @@ class Eitei {
         guard let bundleURL = Bundle(for: Eitei.self).url(forResource: "Resource", withExtension: "bundle"),
               let bundle = Bundle(url: bundleURL) else {
             // 嘗試 SPM 直接讀取
-            let bundle = Bundle(for: type(of: self))
-            if let resourceUrl = bundle.url(forResource: name, withExtension: "png", subdirectory: "Resource.bundle") {
-                if let image = UIImage(contentsOfFile: resourceUrl.path) {
-                    // 在這裡使用
-                    return UIImage(named: name)
+            if let bundleUrl = Bundle.main.url(forResource: "Resource", withExtension: "bundle") {
+                let bundle = Bundle(url: bundleUrl)
+                if let resourceUrl = bundle?.url(forResource: name, withExtension: "png") {
+                    if let image = UIImage(contentsOfFile: resourceUrl.path) {
+                        // 在這裡使用 SPM 導入的 image
+                        return image
+                    }
                 }
             }
             print("讀取bundle失敗")
             return nil
         }
-        // 通过 bundle 载入图片
+        // 通过 CocoaPods 封裝的 bundle 载入图片
         guard let image = UIImage(named: name, in: bundle, compatibleWith: nil) else {
             print("從 \(bundle)讀取圖片 \(name)失敗")
-            // 嘗試 SPM 直接讀取
-            let bundle = Bundle(for: type(of: self))
-            if let resourceUrl = bundle.url(forResource: name, withExtension: "png", subdirectory: "Resource.bundle") {
-                if let image = UIImage(contentsOfFile: resourceUrl.path) {
-                    // 在這裡使用圖片（image）
-                    return UIImage(named: name)
-                }
-            }
-            return UIImage(named: name)
         }
         return image
     }
