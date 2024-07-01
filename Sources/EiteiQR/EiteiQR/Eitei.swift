04ce97f93abd8b363f8d8e95de5b9252f9c8b32e
damao <dadada.maomaomao@gmail.com>
2024-07-01 12:52:19 +0900

    SPM讀取函數封裝

--- Sources/EiteiQR/EiteiQR/Eitei.swift
+++ Sources/EiteiQR/EiteiQR/Eitei.swift
@@ -19,20 +19,31 @@ class Eitei {
         // 获取 CocoaPods 资源 bundle
         guard let bundleURL = Bundle(for: Eitei.self).url(forResource: "Resource", withExtension: "bundle"),
               let bundle = Bundle(url: bundleURL) else {
-            print("讀取bundle失敗")
             // 嘗試 SPM 直接讀取
-            return UIImage(named: name)
+            let bundle = Bundle(for: type(of: self))
+            if let resourceUrl = bundle.url(forResource: name, withExtension: "png", subdirectory: "Resource.bundle") {
+                if let image = UIImage(contentsOfFile: resourceUrl.path) {
+                    // 在這裡使用
+                    return UIImage(named: name)
+                }
+            }
+            print("讀取bundle失敗")
+            return nil
         }
         // 通过 bundle 载入图片
         guard let image = UIImage(named: name, in: bundle, compatibleWith: nil) else {
             print("從 \(bundle)讀取圖片 \(name)失敗")
-            
             // 嘗試 SPM 直接讀取
+            let bundle = Bundle(for: type(of: self))
+            if let resourceUrl = bundle.url(forResource: name, withExtension: "png", subdirectory: "Resource.bundle") {
+                if let image = UIImage(contentsOfFile: resourceUrl.path) {
+                    // 在這裡使用圖片（image）
+                    return UIImage(named: name)
+                }
+            }
             return UIImage(named: name)
         }
-        
         return image
-
     }
     
     
