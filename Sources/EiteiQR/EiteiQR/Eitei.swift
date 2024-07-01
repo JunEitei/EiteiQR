ebfbcc33817e92a1c3aad203814d684d9d54b593
damao <dadada.maomaomao@gmail.com>
2024-07-01 13:45:51 +0900

    fix

--- Sources/EiteiQR/EiteiQR/Eitei.swift
+++ Sources/EiteiQR/EiteiQR/Eitei.swift
@@ -21,27 +21,29 @@ class Eitei {
               let bundle = Bundle(url: bundleURL) else {
             print("讀取bundle失敗")
 #if SWIFT_PACKAGE
-            // 嘗試從文件夾裡直接讀取 （SPM）
             let bundle = Bundle.module
-
-            if let imageUrl = bundle.url(forResource: name, withExtension: "png", subdirectory: "Resource") {
-                if let image = UIImage(contentsOfFile: imageUrl.path) {
-                    // 使用 image
-                    return image
-                }
-            }
-#endif
-            // 保底選項
-            return UIImage(named: name)
             
+            // 這裡假設圖片是 PNG 格式，可以根據實際情況調整 withExtension 的參數
+            if let imageUrl = bundle.url(forResource: name, withExtension: nil, subdirectory: "Resource"),
+               let image = UIImage(contentsOfFile: imageUrl.path) {
+                // 使用 image
+                return image
+            } else {
+                // 如果找不到圖片，可以返回默認圖片或者報錯
+                return UIImage(named: name)
+            }
         }
-        // 通过 CocoaPods 封裝的 bundle 载入图片
-        guard let image = UIImage(named: name, in: bundle, compatibleWith: nil) else {
-            print("從 \(bundle)讀取圖片 \(name)失敗")
-            return nil
-        }
-        return image
+#endif
+        return nil
+        
     }
-    
-    
+    // 通过 CocoaPods 封裝的 bundle 载入图片
+    guard let image = UIImage(named: name, in: bundle, compatibleWith: nil) else {
+        print("從 \(bundle)讀取圖片 \(name)失敗")
+        return nil
+    }
+    return image
+}
+
+
 }
